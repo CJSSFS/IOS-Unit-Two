f0625e5294703716c0f882b8ffe30091814205a4
CJ <cj@iMac7.local>
2019-05-16 14:15:16 -0400

    I was working on how to get a list of players to appear on the screen

---
+++ StatMe/StatMe/Player.swift
@@ -0,0 +1,18 @@
+//
+//  ListOfPlayers.swift
+//  StatMe
+//
+//  Created by CJ on 5/16/19.
+//  Copyright © 2019 CJ. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Player {
+    var name: String
+    var number: Int
+    var shotsmade: Int
+    var shotsattempted: Int
+    
+}
